156884ce4aa53a21e1adac2625a87384503c05c9
温文星 <18475536452@163.com>
2019-03-20 11:31:42 +0800

    更新启动类

--- src/main/java/com/dragon/blog/MyBlogApplication.java
+++ src/main/java/com/dragon/blog/MyBlogApplication.java
@@ -2,6 +2,7 @@ package com.dragon.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author：Dragon Wen
