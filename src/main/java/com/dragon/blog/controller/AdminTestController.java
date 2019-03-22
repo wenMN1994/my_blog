58ad9a85b76d1310d27f6934ee7b704d44f66e10
温文星 <18475536452@163.com>
2019-03-22 16:35:13 +0800

    后台管理页面跳转测试

---
+++ src/main/java/com/dragon/blog/controller/AdminTestController.java
@@ -0,0 +1,34 @@
+package com.dragon.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/3/22 11:50
+ * @description：${description}
+ * @modified By：
+ * @version: $version$
+ */
+@Controller
+@RequestMapping("/admin")
+public class AdminTestController {
+    /**
+     * 测试后台管理跳转
+     * @return
+     */
+    @RequestMapping("")
+    public String admin(){
+        return "admin/index";
+    }
+
+    /**
+     * 测试后台管理跳转
+     * @return
+     */
+    @RequestMapping("/home")
+    public String home(){
+        return "admin/home/home";
+    }
+}
