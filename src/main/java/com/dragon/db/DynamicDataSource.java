314d69a9f34ff2c7cac91103fd0995672edaf20d
温文星 <18475536452@163.com>
2019-04-01 23:26:11 +0800

    动态数据源（数据源切换）

---
+++ src/main/java/com/dragon/db/DynamicDataSource.java
@@ -0,0 +1,57 @@
+package com.dragon.db;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+/**
+ * 
+ * <p>Title: DynamicDataSource</p>  
+ * <p>Description: 动态数据源（数据源切换）</p>  
+ * @author Dragon.Wen
+ * @date Oct 17, 2018
+ */
+public class DynamicDataSource extends AbstractRoutingDataSource {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(DynamicDataSource.class);
+
+    private static final ThreadLocal<String> CONTEXT_HOLDER = new ThreadLocal<>();
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        String dataSource = getDataSource();
+        LOGGER.info("当前操作使用的数据源：{}", dataSource);
+        return dataSource;
+    }
+
+    /**
+     * 设置数据源
+     *
+     * @param dataSource
+     */
+    public static void setDataSource(String dataSource) {
+        CONTEXT_HOLDER.set(dataSource);
+    }
+
+    /**
+     * 获取数据源
+     *
+     * @return
+     */
+    public static String getDataSource() {
+        String dataSource = CONTEXT_HOLDER.get();
+        // 如果没有指定数据源，使用默认数据源
+        if (null == dataSource) {
+            DynamicDataSource.setDataSource(DataSourceEnum.MASTER.getDefault());
+        }
+        return CONTEXT_HOLDER.get();
+    }
+
+    /**
+     * 清除数据源
+     */
+    public static void clearDataSource() {
+        CONTEXT_HOLDER.remove();
+    }
+
+}
