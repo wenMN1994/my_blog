ca14e61587543cc4247cd553ebb8ff1d9d8a3fd0
温文星 <18475536452@163.com>
2019-04-02 14:42:34 +0800

    控制器基类

---
+++ src/main/java/com/dragon/blog/base/BaseController.java
@@ -0,0 +1,37 @@
+package com.dragon.blog.base;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * <p>Title: BaseController</p>  
+ * <p>Description: 控制器基类</p>  
+ * @author Dragon.Wen
+ * @date Oct 17, 2018
+ */
+public abstract class BaseController {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(BaseController.class);
+
+	/**
+	 * 统一异常处理
+	 * @param request
+	 * @param response
+	 * @param exception
+	 */
+	@ExceptionHandler
+	public String exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception exception) {
+		LOGGER.error("统一异常处理：", exception);
+		request.setAttribute("ex", exception);
+		if (null != request.getHeader("X-Requested-With") && "XMLHttpRequest".equalsIgnoreCase(request.getHeader("X-Requested-With"))) {
+			request.setAttribute("requestHeader", "ajax");
+		}
+		return "统一异常处理：";
+	}
+
+}
