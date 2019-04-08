cd60c34009f0b49de6ddbc4922f532854724619b
温文星 <18475536452@163.com>
2019-04-08 23:14:57 +0800

    spring容器初始化完成事件

---
+++ src/main/java/com/dragon/blog/listener/ApplicationContextListener.java
@@ -0,0 +1,62 @@
+package com.dragon.blog.listener;
+
+import com.dragon.blog.annotation.BaseService;
+import com.dragon.blog.base.BaseInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * 
+ * <p>Title: ApplicationContextListener</p>  
+ * <p>Description: spring容器初始化完成事件</p>  
+ * @author Dragon.Wen
+ * @date Oct 18, 2018
+ */
+@Component
+@Lazy
+public class ApplicationContextListener implements ApplicationListener<ContextRefreshedEvent> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationContextListener.class);
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        // root application context
+        if(null == contextRefreshedEvent.getApplicationContext().getParent()) {
+            LOGGER.debug(">>>>> spring初始化完毕 <<<<<");
+            // spring初始化完毕后，通过反射调用所有使用BaseService注解的initMapper方法
+            Map<String, Object> baseServices = contextRefreshedEvent.getApplicationContext().getBeansWithAnnotation(BaseService.class);
+            for(Object service : baseServices.values()) {
+                LOGGER.debug(">>>>> {}.initMapper()", service.getClass().getName());
+                try {
+                    Method initMapper = service.getClass().getMethod("initMapper");
+                    initMapper.invoke(service);
+                } catch (Exception e) {
+                    LOGGER.error("初始化BaseService的initMapper方法异常", e);
+                    e.printStackTrace();
+                }
+            }
+
+            // 系统入口初始化
+            Map<String, BaseInterface> baseInterfaceBeans = contextRefreshedEvent.getApplicationContext().getBeansOfType(BaseInterface.class);
+            for(Object service : baseInterfaceBeans.values()) {
+                LOGGER.debug(">>>>> {}.init()", service.getClass().getName());
+                try {
+                    Method init = service.getClass().getMethod("init");
+                    init.invoke(service);
+                } catch (Exception e) {
+                    LOGGER.error("初始化BaseInterface的init方法异常", e);
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+}
