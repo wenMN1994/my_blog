ca33f98daafa335db52965b9425d16e7fc0b045e
温文星 <18475536452@163.com>
2019-04-08 23:13:20 +0800

    资源文件读取工具

--- src/main/java/com/dragon/blog/utils/SpringContextUtil.java
+++ src/main/java/com/dragon/blog/utils/SpringContextUtil.java
@@ -1,8 +1,9 @@
-package com.dragon.utils;
+package com.dragon.blog.utils;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
@@ -13,6 +14,7 @@ import java.util.Map;
  * @author Dragon.Wen
  * @date Oct 17, 2018
  */
+@Component
 public class SpringContextUtil implements ApplicationContextAware {
 
 	private static ApplicationContext context = null;
