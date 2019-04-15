8903297c301db95202445e692f57e989233ff87d
温文星 <18475536452@163.com>
2019-04-15 16:14:15 +0800

    常量枚举类

---
+++ src/main/java/com/dragon/utils/CustomResult.java
@@ -0,0 +1,19 @@
+package com.dragon.utils;
+
+import com.dragon.base.BaseResult;
+import com.dragon.common.CustomResultConstant;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/4/14 16:40
+ * @description： 常量枚举类
+ * @modified By：
+ * @version: $version$
+ */
+public class CustomResult extends BaseResult {
+
+    public CustomResult(CustomResultConstant customResultConstant, Object data) {
+        super(customResultConstant.getCode(), customResultConstant.getMessage(), data);
+    }
+}
