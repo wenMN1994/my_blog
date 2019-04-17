c0269348c6a252ede9641c761677be8fc40cf8aa
温文星 <18475536452@163.com>
2019-04-17 23:09:37 +0800

    修改访问路径

--- src/main/java/com/dragon/blog/controller/AdminController.java
+++ src/main/java/com/dragon/blog/controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author：Dragon Wen
  * @email：18475536452@163.com
  * @date：Created in 2019/3/22 11:50
- * @description：${description}
+ * @description 后台管理Controller测试
  * @modified By：
  * @version: $version$
  */
@@ -18,16 +18,25 @@ public class AdminController {
      * 测试后台管理跳转
      * @return
      */
-    @RequestMapping("")
+    @RequestMapping("index")
     public String admin(){
         return "admin/index";
+    }
+
+    /**
+     * 测试后台管理登录页面
+     * @return
+     */
+    @RequestMapping("login")
+    public String login(){
+        return "admin/sso/login";
     }
 
     /**
      * 测试后台管理跳转
      * @return
      */
-    @RequestMapping("/home")
+    @RequestMapping("home")
     public String home(){
         return "admin/home/home";
     }
