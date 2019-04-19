140d1e0907e4f514834306b93edc2e4bcbeeef36
温文星 <18475536452@163.com>
2019-04-19 23:13:03 +0800

    创建降级实现BlogSysApiServiceMock接口类

---
+++ src/main/java/com/dragon/blog/service/BlogSysApiServiceMock.java
@@ -0,0 +1,16 @@
+package com.dragon.blog.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/4/19 22:42
+ * @description： 降级实现BlogSysApiServiceMock接口
+ * @modified By：
+ * @version: $version$
+ */
+public class BlogSysApiServiceMock implements BlogSysApiService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BlogSysApiServiceMock.class);
+}
