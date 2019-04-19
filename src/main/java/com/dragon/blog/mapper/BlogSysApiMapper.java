850781d597cec71dd5bfd4e15097d9cb794c7a14
温文星 <18475536452@163.com>
2019-04-19 23:13:43 +0800

    创建用户VOMapper类

---
+++ src/main/java/com/dragon/blog/mapper/BlogSysApiMapper.java
@@ -0,0 +1,13 @@
+package com.dragon.blog.mapper;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/4/19 22:45
+ * @description： 用户VOMapper
+ * @modified By：
+ * @version: $version$
+ */
+public interface BlogSysApiMapper {
+
+}
