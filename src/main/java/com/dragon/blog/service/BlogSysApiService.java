f2bc96d8dcf23ccac3416e9bbe3f364a6d0b4baf
温文星 <18475536452@163.com>
2019-04-19 23:12:32 +0800

    创建Blog系统接口类

---
+++ src/main/java/com/dragon/blog/service/BlogSysApiService.java
@@ -0,0 +1,13 @@
+package com.dragon.blog.service;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/4/19 22:41
+ * @description： Blog系统接口
+ * @modified By：
+ * @version: $version$
+ */
+public interface BlogSysApiService {
+
+}
