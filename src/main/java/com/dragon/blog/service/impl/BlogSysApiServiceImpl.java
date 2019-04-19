a8e1725fd6c244f047e7a2c50cae2087ee1b819e
温文星 <18475536452@163.com>
2019-04-19 23:14:33 +0800

    创建BlogSysApiService实现类

---
+++ src/main/java/com/dragon/blog/service/impl/BlogSysApiServiceImpl.java
@@ -0,0 +1,19 @@
+package com.dragon.blog.service.impl;
+
+import com.dragon.blog.service.BlogSysApiService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/4/19 22:47
+ * @description： BlogSysApiService实现
+ * @modified By：
+ * @version: $version$
+ */
+@Service
+@Transactional
+public class BlogSysApiServiceImpl implements BlogSysApiService {
+
+}
