850e3499ef4fd519c0e7b0e771f58cc57ae219d0
温文星 <18475536452@163.com>
2019-04-24 22:43:06 +0800

    设置shiro不拦截根路径

--- src/main/java/com/dragon/config/ShiroConfig.java
+++ src/main/java/com/dragon/config/ShiroConfig.java
@@ -47,6 +47,7 @@ public class ShiroConfig {
         Map<String,String> filterChainDefinitionMap = new LinkedHashMap<String,String>();
         // 配置不会被拦截的链接 顺序判断
         filterChainDefinitionMap.put("/sso/login", "anon");
+        filterChainDefinitionMap.put("/", "anon");
         //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
         filterChainDefinitionMap.put("/logout", "logout");
         filterChainDefinitionMap.put("/static/**", "anon");
