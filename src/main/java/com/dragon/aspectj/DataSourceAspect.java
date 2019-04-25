7dc67797666482ab4763c0d8eafdccec57efa019
温文星 <18475536452@163.com>
2019-04-25 23:52:28 +0800

    配置多数据源处理

---
+++ src/main/java/com/dragon/aspectj/DataSourceAspect.java
@@ -0,0 +1,56 @@
+package com.dragon.aspectj;
+
+import com.dragon.annotation.DataSource;
+import com.dragon.db.DynamicDataSource;
+import com.dragon.utils.StringUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author：Dragon Wen
+ * @email：18475536452@163.com
+ * @date：Created in 2019/4/25 21:35
+ * @description： 多数据源处理
+ * @modified By：
+ * @version: $version$
+ */
+@Aspect
+@Order(1)
+@Component
+public class DataSourceAspect {
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Pointcut("@annotation(com.dragon.annotation.DataSource)")
+    public void dsPointCut() {
+
+    }
+
+    @Around("dsPointCut()")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        MethodSignature signature = (MethodSignature) point.getSignature();
+
+        Method method = signature.getMethod();
+
+        DataSource dataSource = method.getAnnotation(DataSource.class);
+
+        if (StringUtils.isNotNull(dataSource)) {
+            DynamicDataSource.setDataSource(dataSource.value().name());
+        }
+
+        try {
+            return point.proceed();
+        } finally {
+            // 销毁数据源 在执行方法之后
+            DynamicDataSource.clearDataSource();
+        }
+    }
+}
