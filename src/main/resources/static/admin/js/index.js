dda70ef0542302f607b0cde450e6f2b379b7a224
温文星 <18475536452@163.com>
2019-05-07 15:47:50 +0800

    新增支持全屏显示选项卡

--- src/main/resources/static/admin/js/index.js
+++ src/main/resources/static/admin/js/index.js
@@ -7,4 +7,31 @@ $(function () {
         $('#wrapper').fullScreen();
     });
 
+    // 双击选项卡全屏显示
+    $('.J_menuTabs').on('dblclick', '.J_menuTab', activeTabMax);
+
+    // tab全屏显示
+    $('.J_tabMaxCurrent').on('click', function () {
+        $('.page-tabs-content').find('.active').trigger("dblclick");
+    });
+
+    // 关闭全屏
+    $('#ax_close_max').click(function () {
+        $('#content-main').toggleClass('max');
+        $('#ax_close_max').hide();
+    })
+
+    // 双击选项卡全屏显示
+    function activeTabMax() {
+        $('#content-main').toggleClass('max');
+        $('#ax_close_max').show();
+    }
+
+    $(window).keydown(function (event) {
+        if (event.keyCode == 27) {
+            $('#content-main').removeClass('max');
+            $('#ax_close_max').hide();
+        }
+    });
+
 });
