0cb43c1b5c234dbff86fcb2d1811b045509d5028
温文星 <18475536452@163.com>
2019-05-26 17:24:20 +0800

    缓存管理器-使用Ehcache实现

--- src/main/java/com/dragon/config/ShiroConfig.java
+++ src/main/java/com/dragon/config/ShiroConfig.java
@@ -2,7 +2,12 @@ package com.dragon.config;
 
 import com.dragon.client.shiro.realm.BlogShiroRealm;
 import com.dragon.client.shiro.web.LogoutFilter;
+import com.dragon.utils.StringUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.cache.ehcache.EhCacheManager;
+import org.apache.shiro.config.ConfigurationException;
+import org.apache.shiro.io.ResourceUtils;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
@@ -20,6 +25,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -158,6 +166,41 @@ public class ShiroConfig {
         //自定义session管理，使用redis
         securityManager.setSessionManager(sessionManager());
         return securityManager;
+    }
+
+    /**
+     * 缓存管理器 使用Ehcache实现
+     */
+    @Bean
+    public EhCacheManager getEhCacheManager() {
+        net.sf.ehcache.CacheManager cacheManager = net.sf.ehcache.CacheManager.getCacheManager("dragon");
+        EhCacheManager em = new EhCacheManager();
+        if (StringUtils.isNull(cacheManager)) {
+            em.setCacheManager(new net.sf.ehcache.CacheManager(getCacheManagerConfigFileInputStream()));
+            return em;
+        } else {
+            em.setCacheManager(cacheManager);
+            return em;
+        }
+    }
+
+    /**
+     * 返回配置文件流 避免ehcache配置文件一直被占用，无法完全销毁项目重新部署
+     */
+    protected InputStream getCacheManagerConfigFileInputStream() {
+        String configFile = "classpath:ehcache/ehcache-shiro.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = ResourceUtils.getInputStreamForPath(configFile);
+            byte[] b = IOUtils.toByteArray(inputStream);
+            InputStream in = new ByteArrayInputStream(b);
+            return in;
+        } catch (IOException e) {
+            throw new ConfigurationException(
+                    "Unable to obtain input stream for cacheManagerConfigFile [" + configFile + "]", e);
+        } finally {
+            IOUtils.closeQuietly(inputStream);
+        }
     }
 
     /**
