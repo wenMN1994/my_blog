d7dddfd4ba7da71a3cc84f082e7e6e96dc6c12be
温文星 <18475536452@163.com>
2019-05-28 22:34:25 +0800

    自动生成前端访问日志记录相关类

---
+++ src/main/java/com/dragon/blog/service/BlogFrontVisitLogServiceMock.java
@@ -0,0 +1,14 @@
+package com.dragon.blog.service;
+
+import com.dragon.base.BaseServiceMock;
+import com.dragon.blog.mapper.BlogFrontVisitLogMapper;
+import com.dragon.blog.model.BlogFrontVisitLog;
+import com.dragon.blog.model.BlogFrontVisitLogExample;
+
+/**
+* 降级实现BlogFrontVisitLogService接口
+* Created by Dragon.Wen on 2019/5/27.
+*/
+public class BlogFrontVisitLogServiceMock extends BaseServiceMock<BlogFrontVisitLogMapper, BlogFrontVisitLog, BlogFrontVisitLogExample> implements BlogFrontVisitLogService {
+
+}
