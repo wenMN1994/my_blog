8120191ffcd2f2f97b161df7224b58388b3f3d64
温文星 <18475536452@163.com>
2019-06-01 14:51:53 +0800

    统一请求响应处理

--- src/main/java/com/dragon/base/BaseController.java
+++ src/main/java/com/dragon/base/BaseController.java
@@ -1,11 +1,20 @@
 package com.dragon.base;
 
+import com.dragon.blog.model.BlogSysUser;
+import com.dragon.utils.AjaxResult;
+import com.dragon.utils.DateUtils;
+import com.dragon.utils.StringUtils;
+import com.dragon.utils.security.ShiroUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.beans.PropertyEditorSupport;
+import java.util.Date;
 
 /**
  * 
@@ -32,6 +41,98 @@ public abstract class BaseController {
 			request.setAttribute("requestHeader", "ajax");
 		}
 		return "统一异常处理：";
+	}
+
+	/**
+	 * 将前台传递过来的日期格式的字符串，自动转化为Date类型
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		// Date 类型转换
+		binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+			@Override
+			public void setAsText(String text) {
+				setValue(DateUtils.parseDate(text));
+			}
+		});
+	}
+
+	/**
+	 * 响应返回结果
+	 *
+	 * @param rows 影响行数
+	 * @return 操作结果
+	 */
+	protected AjaxResult toAjax(int rows) {
+		return rows > 0 ? success() : error();
+	}
+
+	/**
+	 * 响应返回结果
+	 *
+	 * @param result 结果
+	 * @return 操作结果
+	 */
+	protected AjaxResult toAjax(boolean result) {
+		return result ? success() : error();
+	}
+
+	/**
+	 * 返回成功
+	 */
+	public AjaxResult success() {
+		return AjaxResult.success();
+	}
+
+	/**
+	 * 返回失败消息
+	 */
+	public AjaxResult error() {
+		return AjaxResult.error();
+	}
+
+	/**
+	 * 返回成功消息
+	 */
+	public AjaxResult success(String message) {
+		return AjaxResult.success(message);
+	}
+
+	/**
+	 * 返回失败消息
+	 */
+	public AjaxResult error(String message) {
+		return AjaxResult.error(message);
+	}
+
+	/**
+	 * 返回错误码消息
+	 */
+	public AjaxResult error(int code, String message) {
+		return AjaxResult.error(code, message);
+	}
+
+	/**
+	 * 页面跳转
+	 */
+	public String redirect(String url) {
+		return StringUtils.format("redirect:{}", url);
+	}
+
+	public BlogSysUser getSysUser() {
+		return ShiroUtils.getSysUser();
+	}
+
+	public void setSysUser(BlogSysUser user) {
+		ShiroUtils.setSysUser(user);
+	}
+
+	public Integer getUserId() {
+		return getSysUser().getUserId();
+	}
+
+	public String getLoginName() {
+		return getSysUser().getLoginName();
 	}
 
 }
