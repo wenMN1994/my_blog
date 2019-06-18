6d52a58dca4809bcb07b4e84c6c806652a4dd520
温文星 <18475536452@163.com>
2019-06-18 23:04:34 +0800

    修改异步任务管理器包结构

--- src/main/java/com/dragon/framework/manager/AsyncManager.java
+++ src/main/java/com/dragon/framework/manager/AsyncManager.java
@@ -1,7 +1,7 @@
-package com.dragon.manager;
+package com.dragon.framework.manager;
 
-import com.dragon.utils.SpringContextUtil;
-import com.dragon.utils.Threads;
+import com.dragon.common.utils.Threads;
+import com.dragon.common.utils.spring.SpringUtils;
 
 import java.util.TimerTask;
 import java.util.concurrent.ScheduledExecutorService;
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 /**
  * @author：Dragon Wen
  * @email：18475536452@163.com
- * @date：Created in 2019/5/27 13:29
+ * @date：Created in 2019/6/17 16:27
  * @description： 异步任务管理器
  * @modified By：
  * @version: 1.0.0
@@ -24,7 +24,7 @@ public class AsyncManager {
     /**
      * 异步操作任务调度线程池
      */
-    private ScheduledExecutorService executor = (ScheduledExecutorService) SpringContextUtil.getBean("scheduledExecutorService");
+    private ScheduledExecutorService executor = SpringUtils.getBean("scheduledExecutorService");
 
     /**
      * 单例模式
