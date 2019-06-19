eb26e46880719e73a5d0bc0f466b02db8e97dec1
温文星 <18475536452@163.com>
2019-06-19 23:20:07 +0800

    修改自定义多数据源切换注解包结构

--- src/main/java/com/dragon/framework/aspectj/lang/annotation/DataSource.java
+++ src/main/java/com/dragon/framework/aspectj/lang/annotation/DataSource.java
@@ -1,6 +1,6 @@
-package com.dragon.annotation;
+package com.dragon.framework.aspectj.lang.annotation;
 
-import com.dragon.db.DataSourceEnum;
+import com.dragon.framework.aspectj.lang.enums.DataSourceType;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -10,10 +10,10 @@ import java.lang.annotation.Target;
 /**
  * @author：Dragon Wen
  * @email：18475536452@163.com
- * @date：Created in 2019/4/25 21:41
+ * @date：Created in 2019/6/17 16:27
  * @description： 自定义多数据源切换注解
  * @modified By：
- * @version: $version$
+ * @version: 1.0.0
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
@@ -21,5 +21,5 @@ public @interface DataSource {
     /**
      * 切换数据源名称
      */
-    DataSourceEnum value() default DataSourceEnum.MASTER;
+     DataSourceType value() default DataSourceType.MASTER;
 }
