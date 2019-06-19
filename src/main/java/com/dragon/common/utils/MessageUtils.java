804740184a4e2cf53c845d75eec0254214fb1bd0
温文星 <18475536452@163.com>
2019-06-19 23:31:12 +0800

    获取i18n资源文件

--- src/main/java/com/dragon/common/utils/MessageUtils.java
+++ src/main/java/com/dragon/common/utils/MessageUtils.java
@@ -1,11 +1,12 @@
-package com.dragon.utils;
+package com.dragon.common.utils;
 
+import com.dragon.common.utils.spring.SpringUtils;
 import org.springframework.context.MessageSource;
 
 /**
  * @author：Dragon Wen
  * @email：18475536452@163.com
- * @date：Created in 2019/5/29 11:06
+ * @date：Created in 2019/6/17 16:27
  * @description： 获取i18n资源文件
  * @modified By：
  * @version: 1.0.0
@@ -19,7 +20,7 @@ public class MessageUtils {
      * @return
      */
     public static String message(String code, Object... args) {
-        MessageSource messageSource = SpringContextUtil.getBean(MessageSource.class);
+        MessageSource messageSource = SpringUtils.getBean(MessageSource.class);
         return messageSource.getMessage(code, args, null);
     }
 }
