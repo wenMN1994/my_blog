2b150598727f0de2aac8a338292e5daf7c389504
温文星 <18475536452@163.com>
2019-06-20 11:05:35 +0800

    update 用户信息异常类

--- src/main/java/com/dragon/common/exception/user/UserException.java
+++ src/main/java/com/dragon/common/exception/user/UserException.java
@@ -5,7 +5,7 @@ import com.dragon.common.exception.base.BaseException;
 /**
  * @author：Dragon Wen
  * @email：18475536452@163.com
- * @date：Created in 2019/5/29 15:37
+ * @date：Created in 2019/6/17 16:27
  * @description： 用户信息异常类
  * @modified By：
  * @version: 1.0.0
