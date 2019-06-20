af9779df773891cd14956b8468b55575769808c5
温文星 <18475536452@163.com>
2019-06-20 10:52:18 +0800

    参数配置

--- src/main/java/com/dragon/framework/web/service/ConfigService.java
+++ src/main/java/com/dragon/framework/web/service/ConfigService.java
@@ -1,21 +1,21 @@
-package com.dragon.web.service;
+package com.dragon.framework.web.service;
 
-import com.dragon.blog.service.BlogSysConfigService;
+import com.dragon.project.system.config.service.IConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * @author：Dragon Wen
  * @email：18475536452@163.com
- * @date：Created in 2019/6/15 15:07
- * @description：html调用 thymeleaf 实现参数管理
+ * @date：Created in 2019/6/17 16:27
+ * @description：  html调用 thymeleaf 实现参数管理
  * @modified By：
  * @version: 1.0.0
  */
 @Service("config")
 public class ConfigService {
     @Autowired
-    private BlogSysConfigService blogSysConfigService;
+    private IConfigService configService;
 
     /**
      * 根据键名查询参数配置信息
@@ -24,7 +24,7 @@ public class ConfigService {
      * @return 参数键值
      */
     public String getKey(String configKey) {
-        return String.valueOf(blogSysConfigService.selectByPrimaryKey(configKey));
+        return configService.selectConfigByKey(configKey);
     }
 
 }
