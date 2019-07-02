5f79ddded9afb121917bb0ebc245aad3d97393b3
温文星 <18475536452@163.com>
2019-07-02 23:58:03 +0800

    更新通知公告业务操作

--- src/main/java/com/dragon/project/system/notice/controller/NoticeController.java
+++ src/main/java/com/dragon/project/system/notice/controller/NoticeController.java
@@ -1,5 +1,6 @@
 package com.dragon.project.system.notice.controller;
 
+import com.dragon.common.utils.poi.ExcelUtil;
 import com.dragon.framework.aspectj.lang.annotation.Log;
 import com.dragon.framework.aspectj.lang.enums.BusinessType;
 import com.dragon.framework.web.controller.BaseController;
@@ -18,7 +19,7 @@ import java.util.List;
 /**
  * @author：Dragon Wen
  * @email：18475536452@163.com
- * @date：Created in 2019/6/17 16:27
+ * @date：Created in 2019/7/02 23:15
  * @description： 公告 信息操作处理
  * @modified By：
  * @version: 1.0.0
@@ -33,77 +34,91 @@ public class NoticeController extends BaseController {
 
     @RequiresPermissions("system:notice:view")
     @GetMapping()
-    public String notice() {
+    public String notice()
+    {
         return prefix + "/notice";
     }
 
     /**
-     * 查询公告列表
+     * 查询通知公告列表
      */
     @RequiresPermissions("system:notice:list")
     @GetMapping("/list")
     @ResponseBody
-    public TableDataInfo list(Notice notice) {
+    public TableDataInfo list(Notice notice)
+    {
         startPage();
         List<Notice> list = noticeService.selectNoticeList(notice);
         return getDataTable(list);
     }
 
+
     /**
-     * 新增公告
+     * 导出通知公告列表
+     */
+    @RequiresPermissions("system:notice:export")
+    @PostMapping("/export")
+    @ResponseBody
+    public AjaxResult export(Notice notice)
+    {
+        List<Notice> list = noticeService.selectNoticeList(notice);
+        ExcelUtil<Notice> util = new ExcelUtil<Notice>(Notice.class);
+        return util.exportExcel(list, "notice");
+    }
+
+    /**
+     * 新增通知公告
      */
     @GetMapping("/add")
-    public String add() {
+    public String add()
+    {
         return prefix + "/add";
     }
 
     /**
-     * 新增保存公告
+     * 新增保存通知公告
      */
     @RequiresPermissions("system:notice:add")
     @Log(title = "通知公告", businessType = BusinessType.INSERT)
     @PostMapping("/add")
     @ResponseBody
-    public AjaxResult addSave(Notice notice) {
+    public AjaxResult addSave(Notice notice)
+    {
         return toAjax(noticeService.insertNotice(notice));
     }
 
     /**
-     * 修改公告
+     * 修改通知公告
      */
     @GetMapping("/edit/{noticeId}")
-    public String edit(@PathVariable("noticeId") Long noticeId, ModelMap mmap) {
-        mmap.put("notice", noticeService.selectNoticeById(noticeId));
+    public String edit(@PathVariable("noticeId") Integer noticeId, ModelMap mmap)
+    {
+        Notice notice = noticeService.selectNoticeById(noticeId);
+        mmap.put("notice", notice);
         return prefix + "/edit";
     }
 
     /**
-     * 修改保存公告
+     * 修改保存通知公告
      */
     @RequiresPermissions("system:notice:edit")
     @Log(title = "通知公告", businessType = BusinessType.UPDATE)
     @PostMapping("/edit")
     @ResponseBody
-    public AjaxResult editSave(Notice notice) {
+    public AjaxResult editSave(Notice notice)
+    {
         return toAjax(noticeService.updateNotice(notice));
     }
 
     /**
-     * 删除公告
+     * 删除通知公告
      */
     @RequiresPermissions("system:notice:remove")
     @Log(title = "通知公告", businessType = BusinessType.DELETE)
-    @DeleteMapping("/remove")
+    @PostMapping( "/remove")
     @ResponseBody
-    public AjaxResult remove(String ids) {
+    public AjaxResult remove(String ids)
+    {
         return toAjax(noticeService.deleteNoticeByIds(ids));
-    }
-
-    @RequiresPermissions("system:notice:edit")
-    @Log(title = "通知公告", businessType = BusinessType.UPDATE)
-    @PutMapping("/changeDisplay/{display}")
-    @ResponseBody
-    public AjaxResult display(Integer id,@PathVariable Integer display) {
-        return toAjax(noticeService.changeNoticeDisplayById(display,id));
     }
 }
