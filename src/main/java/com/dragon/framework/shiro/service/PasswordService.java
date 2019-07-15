3f4dfdbea0247c057bd0c846abd01b69b55b69fe
温文星 <18475536452@163.com>
2019-07-15 14:53:49 +0800

    update

--- src/main/java/com/dragon/framework/shiro/service/PasswordService.java
+++ src/main/java/com/dragon/framework/shiro/service/PasswordService.java
@@ -76,7 +76,6 @@ public class PasswordService {
     }
 
     public static void main(String[] args) {
-        System.out.println(new PasswordService().encryptPassword("admin", "admin123", "111111"));
-        System.out.println(new PasswordService().encryptPassword("ry", "admin123", "222222"));
+        System.out.println(new PasswordService().encryptPassword("admin", "123456", "admin"));
     }
 }
