5a73a1d3f713b7156052b682dc724d1416ab2092
温文星 <18475536452@163.com>
2019-07-27 23:46:23 +0800

    文章详情页面跳转

--- src/main/java/com/dragon/project/front/controller/HomeController.java
+++ src/main/java/com/dragon/project/front/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.dragon.project.front.controller;
 
+import com.dragon.common.constant.CommonConstant;
 import com.dragon.framework.aspectj.lang.annotation.VLog;
 import com.dragon.framework.web.controller.BaseController;
 import com.dragon.project.blog.blog.domain.Blog;
+import com.dragon.project.blog.blog.service.BlogService;
 import com.dragon.project.front.service.HomeService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -26,6 +30,9 @@ public class HomeController extends BaseController {
     @Autowired
     private HomeService homeService;
 
+    @Autowired
+    private BlogService blogService;
+
     @VLog(title = "首页")
     @RequestMapping("")
     public String index(Integer pageNum, Model model){
@@ -36,5 +43,21 @@ public class HomeController extends BaseController {
     }
 
     private void setCommonMessage(Model model) {
+    }
+
+    @VLog(title = "博客")
+    @GetMapping("/f/article/{blogId}.html")
+    public String article(@PathVariable Integer blogId, Model model) {
+        setCommonMessage(model);
+        Blog blog = blogService.selectBlogWithTextAndTagsAndCategoryByBlogId(blogId);
+        //只能访问是已经发表的文章
+        if (!CommonConstant.one.equals(blog.getStatus())) {
+            return "error/404";
+        }
+        model.addAttribute("blog", blog);
+        model.addAttribute("nextBlog", blogService.selectNextBlogById(blogId));
+        model.addAttribute("previousBlog", blogService.selectPreviousBlogById(blogId));
+        model.addAttribute("randBlogList", blogService.selectRandBlogList());
+        return "front/article/article";
     }
 }
