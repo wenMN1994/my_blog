28929830bf19159d7c08e1861909da4826f965f1
温文星 <18475536452@163.com>
2019-08-22 15:36:15 +0800

    后台评论管理实现

--- src/main/java/com/dragon/project/blog/comments/controller/CommentsController.java
+++ src/main/java/com/dragon/project/blog/comments/controller/CommentsController.java
@@ -2,13 +2,19 @@ package com.dragon.project.blog.comments.controller;
 
 import com.dragon.framework.web.controller.BaseController;
 import com.dragon.framework.web.page.TableDataInfo;
+import com.dragon.project.blog.comments.domain.CommentsInfo;
+import com.dragon.project.blog.comments.service.CommentsInfoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author：Dragon Wen
@@ -18,15 +24,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @modified By：
  * @version: 1.0.0
  */
+@Api("评论管理")
 @Controller
 @RequestMapping("/blog/comments")
-@Api("评论管理")
 public class CommentsController extends BaseController {
     private static Logger logger = LoggerFactory.getLogger(CommentsController.class);
 
     private String prefix = "blog/comments";
 
-    @GetMapping("/")
+    @Autowired
+    private CommentsInfoService commentsInfoService;
+
+    @GetMapping()
     @ApiOperation("访问评论管理页面")
     public String comments() {
         return prefix + "/comments";
@@ -34,8 +43,11 @@ public class CommentsController extends BaseController {
 
     @GetMapping("/list")
     @ApiOperation("查询评论信息")
-    public TableDataInfo list() {
-        return null;
+    @ResponseBody
+    public TableDataInfo list(CommentsInfo commentsInfo) {
+        startPage();
+        List<CommentsInfo> commentsInfoList = commentsInfoService.selectCommentsInfoList(commentsInfo);
+        return getDataTable(commentsInfoList);
     }
 
 }
