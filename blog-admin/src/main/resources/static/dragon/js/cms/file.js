cd27b9ab0c51fe65e3a5abb838b2e7cd91895e01
DragonWen <18475536452@163.com>
2021-09-24 22:57:45 +0800

    文件列表布局

--- blog-admin/src/main/resources/static/dragon/js/cms/file.js
+++ blog-admin/src/main/resources/static/dragon/js/cms/file.js
@@ -12,86 +12,38 @@ $(function() {
         };
         $('#scroll-up').toTop(opt);
     }
+    $('.file-box').each(function () {
+        animationHover(this, 'pulse');
+    });
     queryFileList();
 });
+
+/**
+ *
+ * @param element
+ * @param animation
+ */
+function animationHover(element, animation) {
+    element = $(element);
+    element.hover(
+        function () {
+            element.addClass('animated ' + animation);
+        },
+        function () {
+            //动画完成之前移除class
+            window.setTimeout(function () {
+                element.removeClass('animated ' + animation);
+            }, 2000);
+        }
+    );
+}
+
 
 /**
  * 查询文件列表
  */
 function queryFileList() {
-    var options = {
-        url: prefix + "/list",
-        createUrl: prefix + "/add",
-        updateUrl: prefix + "/edit/{id}",
-        removeUrl: prefix + "/remove",
-        exportUrl: prefix + "/export",
-        importUrl: prefix + "/importData",
-        importTemplateUrl: prefix + "/importTemplate",
-        sortName: "createTime",
-        sortOrder: "desc",
-        modalName: "用户",
-        columns: [{
-            checkbox: true
-        },
-            {
-                field: 'userId',
-                title: '用户ID'
-            },
-            {
-                field: 'loginName',
-                title: '登录名称',
-                sortable: true
-            },
-            {
-                field: 'userName',
-                title: '用户名称'
-            },
-            {
-                field: 'dept.deptName',
-                title: '部门'
-            },
-            {
-                field: 'email',
-                title: '邮箱',
-                visible: false
-            },
-            {
-                field: 'phonenumber',
-                title: '手机'
-            },
-            {
-                visible: editFlag == 'hidden' ? false : true,
-                title: '用户状态',
-                align: 'center',
-                formatter: function (value, row, index) {
-                    return statusTools(row);
-                }
-            },
-            {
-                field: 'createTime',
-                title: '创建时间',
-                sortable: true
-            },
-            {
-                title: '操作',
-                align: 'center',
-                formatter: function(value, row, index) {
-                    if (row.userId != 1) {
-                        var actions = [];
-                        actions.push('<a class="btn btn-success btn-xs ' + editFlag + '" href="javascript:void(0)" onclick="$.operate.editTab(\'' + row.userId + '\')"><i class="fa fa-edit"></i>编辑</a> ');
-                        actions.push('<a class="btn btn-danger btn-xs ' + removeFlag + '" href="javascript:void(0)" onclick="$.operate.remove(\'' + row.userId + '\')"><i class="fa fa-remove"></i>删除</a> ');
-                        var more = [];
-                        more.push("<a class='btn btn-default btn-xs " + resetPwdFlag + "' href='javascript:void(0)' onclick='resetPwd(" + row.userId + ")'><i class='fa fa-key'></i>重置密码</a> ");
-                        more.push("<a class='btn btn-default btn-xs " + editFlag + "' href='javascript:void(0)' onclick='authRole(" + row.userId + ")'><i class='fa fa-check-square-o'></i>分配角色</a>");
-                        actions.push('<a tabindex="0" class="btn btn-info btn-xs" role="button" data-container="body" data-placement="left" data-toggle="popover" data-html="true" data-trigger="hover" data-content="' + more.join('') + '"><i class="fa fa-chevron-circle-right"></i>更多操作</a>');
-                        return actions.join('');
-                    } else {
-                        return "";
-                    }
-                }
-            }]
-    };
-    $.table.init(options);
+
 }
 
 /**
@@ -108,3 +60,25 @@ $(".nav.ul-onclick").on("click", "li", function() {
     $(this).parent().children(".li-active").removeClass("li-active");
     $(this).addClass("li-active");
 });
+
+/**
+ * 预览文件
+ * @param obj
+ * @param fileId
+ */
+function viewFile(obj, fileId) {
+    $(obj).parent().addClass("file-active");
+    $(obj).parent().parent().siblings().children().removeClass("file-active");
+    alert("预览文件！");
+}
+
+/**
+ * 删除文件
+ * @param obj
+ * @param fileId
+ */
+function deleteFile(obj, fileId) {
+    $(obj).parent().addClass("file-active");
+    $(obj).parent().parent().siblings().children().removeClass("file-active");
+    alert("删除文件！");
+}
