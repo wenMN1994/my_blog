ed3e36598e8f6e605f1f26bd6e77acbcfa2c45cb
温文星 <18475536452@163.com>
2022-06-22 16:21:00 +0800

    删除多余字段

--- blog-common/src/main/java/com/dragon/common/core/domain/entity/SysUser.java
+++ blog-common/src/main/java/com/dragon/common/core/domain/entity/SysUser.java
@@ -56,9 +56,6 @@ public class SysUser extends BaseEntity
     /** 密码 */
     private String password;
 
-    /** 盐加密 */
-    private String salt;
-
     /** 帐号状态（0正常 1停用） */
     @Excel(name = "帐号状态", readConverterExp = "0=正常,1=停用")
     private String status;
@@ -199,16 +196,6 @@ public class SysUser extends BaseEntity
         this.password = password;
     }
 
-    public String getSalt()
-    {
-        return salt;
-    }
-
-    public void setSalt(String salt)
-    {
-        this.salt = salt;
-    }
-
     public String getStatus()
     {
         return status;
@@ -291,7 +278,6 @@ public class SysUser extends BaseEntity
             .append("avatar", getAvatar())
             .append("avatarId", getAvatarId())
             .append("password", getPassword())
-            .append("salt", getSalt())
             .append("status", getStatus())
             .append("delFlag", getDelFlag())
             .append("loginIp", getLoginIp())
