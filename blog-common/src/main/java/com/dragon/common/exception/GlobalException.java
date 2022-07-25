4f698ed5feeff8f279d5a53dd855196213130305
温文星 <18475536452@163.com>
2022-07-25 22:00:04 +0800

    防止主键字段名与'row'或'ids'一致导致报错的问题

--- blog-common/src/main/java/com/dragon/common/exception/GlobalException.java
+++ blog-common/src/main/java/com/dragon/common/exception/GlobalException.java
@@ -5,8 +5,7 @@ package com.dragon.common.exception;
  * 
  * @author dragon
  */
-public class GlobalException extends RuntimeException
-{
+public class GlobalException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
@@ -25,8 +24,7 @@ public class GlobalException extends RuntimeException
     /**
      * 空构造方法，避免反序列化问题
      */
-    public GlobalException()
-    {
+    public GlobalException() {
     }
 
     public GlobalException(String message)
@@ -34,25 +32,21 @@ public class GlobalException extends RuntimeException
         this.message = message;
     }
 
-    public String getDetailMessage()
-    {
+    public String getDetailMessage() {
         return detailMessage;
     }
 
-    public GlobalException setDetailMessage(String detailMessage)
-    {
+    public GlobalException setDetailMessage(String detailMessage) {
         this.detailMessage = detailMessage;
         return this;
     }
 
     @Override
-    public String getMessage()
-    {
+    public String getMessage() {
         return message;
     }
 
-    public GlobalException setMessage(String message)
-    {
+    public GlobalException setMessage(String message) {
         this.message = message;
         return this;
     }
