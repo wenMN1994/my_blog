b076f324b537e84a69ae7f6bb139fcb7bc563897
温文星 <18475536452@163.com>
2022-12-20 22:04:56 +0800

    修改参数键名时移除前缓存配置

--- blog-system/src/main/java/com/dragon/system/mapper/SysConfigMapper.java
+++ blog-system/src/main/java/com/dragon/system/mapper/SysConfigMapper.java
@@ -6,21 +6,29 @@ import java.util.List;
 
 /**
  * 参数配置 数据层
- * 
+ *
  * @author dragon
  */
 public interface SysConfigMapper {
     /**
      * 查询参数配置信息
-     * 
+     *
      * @param config 参数配置信息
      * @return 参数配置信息
      */
     public SysConfig selectConfig(SysConfig config);
 
     /**
+     * 通过ID查询配置
+     *
+     * @param configId 参数ID
+     * @return 参数配置信息
+     */
+    public SysConfig selectConfigById(Long configId);
+
+    /**
      * 查询参数配置列表
-     * 
+     *
      * @param config 参数配置信息
      * @return 参数配置集合
      */
@@ -28,7 +36,7 @@ public interface SysConfigMapper {
 
     /**
      * 根据键名查询参数配置信息
-     * 
+     *
      * @param configKey 参数键名
      * @return 参数配置信息
      */
@@ -36,7 +44,7 @@ public interface SysConfigMapper {
 
     /**
      * 新增参数配置
-     * 
+     *
      * @param config 参数配置信息
      * @return 结果
      */
@@ -44,7 +52,7 @@ public interface SysConfigMapper {
 
     /**
      * 修改参数配置
-     * 
+     *
      * @param config 参数配置信息
      * @return 结果
      */
@@ -52,7 +60,7 @@ public interface SysConfigMapper {
 
     /**
      * 删除参数配置
-     * 
+     *
      * @param configId 参数ID
      * @return 结果
      */
@@ -60,7 +68,7 @@ public interface SysConfigMapper {
 
     /**
      * 批量删除参数信息
-     * 
+     *
      * @param configIds 需要删除的参数ID
      * @return 结果
      */
