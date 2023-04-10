44661c41fc45582ce75c4ca8c52da162d8e84f08
DragonWen <18475536452@163.com>
2023-04-10 21:43:37 +0800

    导出Excel，@Excel注解使用dictType属性时，如果有大量的字典数据，就会有大量的查询redis(打开、关闭)，导致特别慢。于是使用map存储字典数据，相同的key就不需要再次去查询redis，大大提高了导出效率。

--- blog-common/src/main/java/com/dragon/common/utils/poi/ExcelUtil.java
+++ blog-common/src/main/java/com/dragon/common/utils/poi/ExcelUtil.java
@@ -79,6 +79,12 @@ public class ExcelUtil<T> {
     public static final String[] FORMULA_STR = { "=", "-", "+", "@" };
 
     /**
+     * 导出Excel时，如果有大量的字典数据，就会有大量的查询redis(打开、关闭)，导致特别慢。
+     * 于是使用map存储字典数据，相同的key就不需要再次去查询redis
+     */
+    public Map<String,String> sysDictMap = new HashMap<String,String>();
+
+    /**
      * Excel sheet最大行数，默认65536
      */
     public static final int sheetSize = 65536;
@@ -866,7 +872,11 @@ public class ExcelUtil<T> {
                 } else if (StringUtils.isNotEmpty(readConverterExp) && StringUtils.isNotNull(value)) {
                     cell.setCellValue(convertByExp(Convert.toStr(value), readConverterExp, separator));
                 } else if (StringUtils.isNotEmpty(dictType) && StringUtils.isNotNull(value)) {
-                    cell.setCellValue(convertDictByExp(Convert.toStr(value), dictType, separator));
+                    if (!sysDictMap.containsKey(dictType+value)) {
+                        String label = convertDictByExp(Convert.toStr(value), dictType, separator);
+                        sysDictMap.put(dictType+value,label);
+                    }
+                    cell.setCellValue(sysDictMap.get(dictType+value));
                 } else if (value instanceof BigDecimal && -1 != attr.scale()) {
                     cell.setCellValue((((BigDecimal) value).setScale(attr.scale(), attr.roundingMode())).doubleValue());
                 } else if (!attr.handler().equals(ExcelHandlerAdapter.class)) {
