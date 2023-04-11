5a7130e264acaaa15be2bd82a5296ad1bd53a079
DragonWen <18475536452@163.com>
2023-04-11 22:21:46 +0800

    将el-scrollbar移动到main-container下，避免鼠标移出时无法隐藏的问题

--- admin-ui/src/layout/index.vue
+++ admin-ui/src/layout/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div :class="classObj" class="app-wrapper" :style="{'--current-color': theme}">
-    <el-scrollbar>
-      <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"/>
-      <sidebar v-if="!sidebar.hide" class="sidebar-container"/>
-      <div :class="{hasTagsView:needTagsView,sidebarHide:sidebar.hide}" class="main-container">
+    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside"/>
+    <sidebar v-if="!sidebar.hide" class="sidebar-container"/>
+    <div :class="{hasTagsView:needTagsView,sidebarHide:sidebar.hide}" class="main-container">
+      <el-scrollbar>
         <div :class="{'fixed-header':fixedHeader}">
           <navbar/>
           <tags-view v-if="needTagsView"/>
@@ -12,8 +12,8 @@
         <right-panel>
           <settings/>
         </right-panel>
-      </div>
-    </el-scrollbar>
+      </el-scrollbar>
+    </div>
   </div>
 </template>
 
