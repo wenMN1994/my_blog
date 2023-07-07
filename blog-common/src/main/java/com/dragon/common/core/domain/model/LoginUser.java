3ee237d4378ff3fff41996d218360a2fe08459cc
DragonWen <18475536452@163.com>
2023-07-07 23:44:01 +0800

    格式化代码

--- blog-common/src/main/java/com/dragon/common/core/domain/model/LoginUser.java
+++ blog-common/src/main/java/com/dragon/common/core/domain/model/LoginUser.java
@@ -66,6 +66,20 @@ public class LoginUser implements UserDetails {
      */
     private SysUser user;
 
+    public LoginUser() {
+    }
+
+    public LoginUser(SysUser user, Set<String> permissions) {
+        this.user = user;
+        this.permissions = permissions;
+    }
+
+    public LoginUser(Long userId, SysUser user, Set<String> permissions) {
+        this.userId = userId;
+        this.user = user;
+        this.permissions = permissions;
+    }
+
     public Long getUserId() {
         return userId;
     }
@@ -80,20 +94,6 @@ public class LoginUser implements UserDetails {
 
     public void setToken(String token) {
         this.token = token;
-    }
-
-    public LoginUser() {
-    }
-
-    public LoginUser(SysUser user, Set<String> permissions) {
-        this.user = user;
-        this.permissions = permissions;
-    }
-
-    public LoginUser(Long userId, SysUser user, Set<String> permissions) {
-        this.userId = userId;
-        this.user = user;
-        this.permissions = permissions;
     }
 
     @JSONField(serialize = false)
