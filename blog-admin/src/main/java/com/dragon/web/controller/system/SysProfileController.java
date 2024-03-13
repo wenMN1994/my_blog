548c7afd9f35d5506d38412db30bdb640eee8967
DragonWen <18475536452@163.com>
2024-03-13 21:44:17 +0800

    优化个人中心/基本资料修改时数据显示问题

--- blog-admin/src/main/java/com/dragon/web/controller/system/SysProfileController.java
+++ blog-admin/src/main/java/com/dragon/web/controller/system/SysProfileController.java
@@ -64,10 +64,10 @@ public class SysProfileController extends BaseController {
         currentUser.setPhonenumber(user.getPhonenumber());
         currentUser.setSex(user.getSex());
         if (StringUtils.isNotEmpty(user.getPhonenumber()) && !userService.checkPhoneUnique(currentUser)) {
-            return error("修改用户'" + user.getUserName() + "'失败，手机号码已存在");
+            return error("修改用户'" + loginUser.getUsername() + "'失败，手机号码已存在");
         }
         if (StringUtils.isNotEmpty(user.getEmail()) && !userService.checkEmailUnique(currentUser)) {
-            return error("修改用户'" + user.getUserName() + "'失败，邮箱账号已存在");
+            return error("修改用户'" + loginUser.getUsername() + "'失败，邮箱账号已存在");
         }
         if (userService.updateUserProfile(currentUser) > 0) {
             // 更新缓存用户信息
